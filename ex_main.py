b1b55863c4d9446d555d658486f4e055d131ccdd
ak-lucas <akayamalucas1@gmail.com>
2017-09-09 17:42:57 -0400

    exemplo de uso

---
+++ ex_main.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+from regressao_logistica import LogisticRegression
+import csv
+import numpy as np
+from sklearn.metrics import accuracy_score
+import time
+
+#PREPARAÇÃO DO DATASET
+X = []
+Y = []
+with open('iris_mod.csv', 'r') as f:
+	reader = csv.reader(f)
+	for r in reader:
+		x = r[:-1]
+		X.append([float(a) for a in x])
+		Y.append(int(r[-1]))
+
+X = np.array(X)
+Y = np.array(Y)
+
+indices = np.arange(X.shape[0])
+
+np.random.shuffle(indices)
+
+X = X[indices]
+Y = Y[indices]
+
+TRAIN_SIZE = int(.8 * X.shape[0])
+
+X_train = X[:TRAIN_SIZE]
+Y_train = Y[:TRAIN_SIZE]
+
+X_test = X[TRAIN_SIZE:]
+Y_test = Y[TRAIN_SIZE:]
+
+#REGRESSÃO LOGÍSTICA
+LR = LogisticRegression()
+
+LR.fit(X_train,Y_train, epochs=30, learning_rate=0.08, print_results=False)
+print "train accuracy: " + str(LR.accuracy_score(X_train,Y_train)*100.0) + "%"
+
+Y_predict = LR.predict(X_test)
+
+print 'test accuracy: ' + str(LR.accuracy_score(X_test,Y_test)*100.0) + "%"
+#print accuracy_score(Y_test,Y_predict)	#sklearn accuracy
+
+print 'final loss: ' + str(LR.loss[-1])
