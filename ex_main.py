0058042a123e3299669a2d47e06b6d0ec0bc37c1
Lucas <31487851+ak-lucas@users.noreply.github.com>
2017-09-09 18:09:06 -0400

    Update ex_main.py

--- ex_main.py
+++ ex_main.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-from regressao_logistica import LogisticRegression
+from regressao_logistica import LogisticRegression	#colocar regressao_logistica.py na mesma pasta
 import csv
 import numpy as np
 from sklearn.metrics import accuracy_score
